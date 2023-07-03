61d4a1be847d2b49e4412a204df379358c6b60ae
Rezaul Hoque <rezaulhoque0101@gmail.com>
2023-07-03 07:42:34 +0600

    [Done] Api/Route nout found error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewars/globalErrorHandler';
 import routes from './app/routes';
+import httpStatus from 'http-status';
 const app: Application = express();
 
 // cors setup
@@ -20,5 +21,20 @@ app.use('/api/v1/', routes);
 
 //global error handler
 app.use(globalErrorHandler);
+
+//handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: 'false',
+    message: 'Nout Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'Api Not Found',
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
